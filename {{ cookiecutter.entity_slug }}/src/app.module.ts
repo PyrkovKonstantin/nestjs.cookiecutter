ba2fdc5ed5ad60f152ec72e2ba88a55fe9b6e9fe
kopyrkov <kopyrkov@greenatom.ru>
2024-10-21 12:35:03 +0300

    feat: init

---
+++ {{ cookiecutter.entity_slug }}/src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app/app.controller';
+import { AmqpBrokerModule } from './modules/amqp-broker/amqp-broker.module';
+
+@Module({
+  imports: [AmqpBrokerModule],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
