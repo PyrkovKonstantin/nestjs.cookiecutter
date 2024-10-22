adc82966fa4265b36be573ab9e74807b73fb84d0
kopyrkov <kopyrkov@greenatom.ru>
2024-10-22 10:51:29 +0300

    fix

--- {{ cookiecutter.entity_slug }}/src/app.module.ts
+++ {{ cookiecutter.entity_slug }}/src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app/app.controller';
 import { AmqpBrokerModule } from './modules/amqp-broker/amqp-broker.module';
+{%- if cookiecutter.use_db == 'y' %}
 import TypeOrmConfigService from './config/orm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+{%- else -%}
+//
+{%- endif %}
 
 @Module({
   imports: [
     AmqpBrokerModule,
-    TypeOrmModule.forRootAsync({
-      useClass: TypeOrmConfigService,
-    }),
+    {%- if cookiecutter.use_db == 'y' %}
+      TypeOrmModule.forRootAsync({
+        useClass: TypeOrmConfigService,
+      }),
+      {%- else -%}
+      //
+      {%- endif %}
   ],
   controllers: [AppController],
   providers: [],
